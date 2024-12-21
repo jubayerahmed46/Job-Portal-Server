f5ee2f3496ed6bb6014506e48c081d8607cadffc
jubayer <jubayerahmed46@gmail.com>
2024-12-21 11:49:09 +0600

    sequre and used jwt

--- app.js
+++ app.js
@@ -1,13 +1,47 @@
 const express = require("express");
 const { MongoClient, ObjectId } = require("mongodb");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT;
 // middlewares
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://job-portal-d5361.web.app/",
+      "https://job-portal-d5361.firebaseapp.com/",
+    ],
+    credentials: true,
+  })
+);
+app.use(cookieParser());
 app.use(express.json());
+
+// verify token middleware
+const varifyToken = (req, res, next) => {
+  if (!req.query.email) {
+    next();
+    return;
+  }
+  const token = req.cookies.token;
+
+  if (!token) {
+    return res.status(401).send({ message: "Unauthorized Error" });
+  }
+
+  jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "Unauthorized Error" });
+    }
+
+    req.user = decoded;
+    next();
+  });
+};
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0shnp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 const client = new MongoClient(uri);
@@ -20,14 +54,42 @@ const client = new MongoClient(uri);
     const jobsCollection = await database.collection("jobs");
     const applicationsCollection = await database.collection("applications");
 
+    // create jwt token
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.SECRET_KEY);
+
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "Strict",
+        })
+        .send({ success: true });
+    });
+
+    app.post("/logout", (req, res) => {
+      res
+        .clearCookie("token", {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          maxAge: 0,
+        })
+        .send({ message: "Logget out" });
+    });
+
     // get all running jobs || get reqruter data using query param
-    app.get("/jobs", async (req, res) => {
+    app.get("/jobs", varifyToken, async (req, res) => {
       try {
         let query = {};
         let default_limit = 8;
 
         if (req.query.email) {
           query = { hr_email: req.query.email };
+
+          if (req.user.email !== req.query.email) {
+            return res.status(403).send({ message: "Forbidden" });
+          }
         }
 
         const jobs = await jobsCollection
@@ -40,7 +102,7 @@ const client = new MongoClient(uri);
         }
         res.send(jobs);
       } catch (error) {
-        res.send(`server error : ${error}`);
+        res.status(500).send({ message: "Internal server error" });
       }
     });
 
@@ -54,7 +116,7 @@ const client = new MongoClient(uri);
 
         res.send(result);
       } catch (error) {
-        res.send(`server error : ${error}`);
+        res.status(500).send({ message: "Internal server error" });
       }
     });
 
@@ -67,7 +129,7 @@ const client = new MongoClient(uri);
         const result = await applicationsCollection.find(query).toArray();
         res.send(result);
       } catch (error) {
-        res.send(`server error : ${error}`);
+        res.status(500).send({ message: "Internal server error" });
       }
     });
 
@@ -95,7 +157,7 @@ const client = new MongoClient(uri);
 
         res.send(result);
       } catch (error) {
-        res.send(`server error : ${error}`);
+        res.status(500).send({ message: "Internal server error" });
       }
     });
 
@@ -129,7 +191,7 @@ const client = new MongoClient(uri);
 
         res.send(applyJob);
       } catch (error) {
-        res.send(`server error : ${error}`);
+        res.status(500).send({ message: "Internal server error" });
       }
     });
 
@@ -145,9 +207,13 @@ const client = new MongoClient(uri);
     });
 
     // load all application of current user/reqruter
-    app.get("/my-applications", async (req, res) => {
+    app.get("/my-applications", varifyToken, async (req, res) => {
       try {
         const email = req.query.email;
+
+        if (req.user.email !== email) {
+          return res.status(403).send({ message: "Forbidden" });
+        }
         const query = { email };
         const applications = await applicationsCollection.find(query).toArray();
 
